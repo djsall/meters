3fd213250e185b4d98319ca55a76c23f301ca8fc
Levente Proksa <proksalevente@gmail.com>
2024-06-22 19:40:12 +0200

    move delete action

--- app/Filament/Resources/ReadingResource.php
+++ app/Filament/Resources/ReadingResource.php
@@ -67,8 +67,10 @@ class ReadingResource extends Resource
                 //
             ])
             ->actions([
-                Tables\Actions\EditAction::make(),
-                Tables\Actions\DeleteAction::make(),
+                Tables\Actions\EditAction::make()
+                    ->extraModalFooterActions([
+                        Tables\Actions\DeleteAction::make(),
+                    ]),
             ])
             ->bulkActions([
                 Tables\Actions\BulkActionGroup::make([

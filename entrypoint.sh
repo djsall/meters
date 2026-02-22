0c317d1d04fb50ebd739f58a8eb33cc086718723
Levente Proksa <proksalevente@gmail.com>
2026-02-22 09:13:26 +0100

    wip

---
+++ entrypoint.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+# Clear and rebuild caches
+php artisan cache:clear
+php artisan config:cache
+php artisan route:cache
+php artisan view:cache
+
+# Restart Queues (If using Horizon or standard workers)
+php artisan queue:restart
+
+# Run migrations (Optional: use --force for production)
+php artisan migrate --force
+
+exec "$@"
